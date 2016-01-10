c9113d9ff544e294a8556609918ff5cb03e1fdca
Hein Htat <heinthuhtat@gmail.com>
2016-01-09 23:30:17 -0800

    Initial commit

---
+++ BurmeseVirtualKeyboard/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace BurmeseVirtualKeyboard
+{
+  public partial class MainWindow : Window
+  {
+    private const string keyCharactersZawgyi = "ကခဂဃငစဆဇဈဉညဋဌဍဎဏတထဒဓနပဖဗဘမယရ႐လဝသႆဟဠအ၏ဤဥဦဧဩဪ၌၍၎႑႒ဣါၚာိီုူေဲဳဴွံ့း္်ၼြၱၶၻ၀၁၂၃၄၅၆၇၈၉၊။ၽၾၿႀႁႂႃႄျ႔႕႖႗ၤၦၧၱၲၷ႖ၼဤ၌ၸၠဉ၍ၪႆၥၰဈၺၽႇႎႌႃႄႉႍႋၵၶၹၨၳၴၡၣႅၻၫၩႁႂ";
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      addKeyButtons(keyCharactersZawgyi, 16);
+    }
+
+    private void addKeyButtons(string keyCharacters, int keysPerRow)
+    {
+      int numRows = (keyCharacters.Length + keysPerRow - 1) / keysPerRow;
+
+      keyboardGrid.Children.Clear();
+      keyboardGrid.RowDefinitions.Clear();
+      keyboardGrid.ColumnDefinitions.Clear();
+
+      for (int i = 0; i < numRows; i++)
+      {
+        keyboardGrid.RowDefinitions.Add(new RowDefinition());
+      }
+
+      for (int i = 0; i < keysPerRow; i++)
+      {
+        keyboardGrid.ColumnDefinitions.Add(new ColumnDefinition());
+      }
+
+      for (int i = 0; i < keyCharacters.Length; i++)
+      {
+        char character = keyCharacters[i];
+
+        Button button = new Button()
+        {
+          Content = character,
+          Background = new SolidColorBrush(Colors.Black),
+          Foreground = new SolidColorBrush(Colors.White),
+          BorderThickness = new Thickness(),
+        };
+
+        Grid.SetRow(button, i / keysPerRow);
+        Grid.SetColumn(button, i % keysPerRow);
+
+        button.Click += (object sender, RoutedEventArgs e) =>
+        {
+          typeCharacter(character);
+        };
+
+        keyboardGrid.Children.Add(button);
+      }
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      WindowInteropHelper interopHelper = new WindowInteropHelper(this);
+
+      NativeMethods.WindowStyle style = NativeMethods.GetWindowLong(
+        interopHelper.Handle,
+        NativeMethods.WindowLong.GWL_EXSTYLE);
+
+      style |= NativeMethods.WindowStyle.WS_EX_NOACTIVATE;
+
+      NativeMethods.SetWindowLong(interopHelper.Handle,
+        NativeMethods.WindowLong.GWL_EXSTYLE,
+        style);
+    }
+
+    private static void typeCharacter(char character)
+    {
+      NativeMethods.KeyboardInput keyboardInputDown = new NativeMethods.KeyboardInput
+      {
+        wVk = 0,
+        wScan = character,
+        dwFlags = NativeMethods.KeyboardInputFlags.KEYEVENTF_UNICODE,
+        time = 0,
+        dwExtraInfo = UIntPtr.Zero,
+      };
+
+      sendKeboyardInput(keyboardInputDown);
+
+      NativeMethods.KeyboardInput keyboardInputUp = new NativeMethods.KeyboardInput
+      {
+        wVk = 0,
+        wScan = character,
+        dwFlags = NativeMethods.KeyboardInputFlags.KEYEVENTF_UNICODE | NativeMethods.KeyboardInputFlags.KEYEVENTF_KEYUP,
+        time = 0,
+        dwExtraInfo = UIntPtr.Zero,
+      };
+
+      sendKeboyardInput(keyboardInputUp);
+    }
+
+    private static void sendKeboyardInput(NativeMethods.KeyboardInput keyboardInput)
+    {
+      NativeMethods.Input input = new NativeMethods.Input
+      {
+        type = NativeMethods.InputType.INPUT_KEYBOARD,
+        ki = keyboardInput,
+      };
+
+      NativeMethods.SendInput(
+        1,
+        new NativeMethods.Input[] { input },
+        Marshal.SizeOf(typeof(NativeMethods.Input)));
+    }
+  }
+}
---
+++ BurmeseVirtualKeyboard/NativeMethods.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BurmeseVirtualKeyboard
+{
+  internal class NativeMethods
+  {
+    [Flags]
+    internal enum WindowStyle : int
+    {
+      None = 0,
+      WS_EX_ACCEPTFILES = 0x00000010,
+      WS_EX_APPWINDOW = 0x00040000,
+      WS_EX_CLIENTEDGE = 0x00000200,
+      WS_EX_COMPOSITED = 0x02000000,
+      WS_EX_CONTEXTHELP = 0x00000400,
+      WS_EX_CONTROLPARENT = 0x00010000,
+      WS_EX_DLGMODALFRAME = 0x00000001,
+      WS_EX_LAYERED = 0x00080000,
+      WS_EX_LAYOUTRTL = 0x00400000,
+      WS_EX_LEFT = 0x00000000,
+      WS_EX_LEFTSCROLLBAR = 0x00004000,
+      WS_EX_LTRREADING = 0x00000000,
+      WS_EX_MDICHILD = 0x00000040,
+      WS_EX_NOACTIVATE = 0x08000000,
+      WS_EX_NOINHERITLAYOUT = 0x00100000,
+      WS_EX_NOPARENTNOTIFY = 0x00000004,
+      WS_EX_NOREDIRECTIONBITMAP = 0x00200000,
+      WS_EX_OVERLAPPEDWINDOW = WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE,
+      WS_EX_PALETTEWINDOW = WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST,
+      WS_EX_RIGHT = 0x00001000,
+      WS_EX_RIGHTSCROLLBAR = 0x00000000,
+      WS_EX_RTLREADING = 0x00002000,
+      WS_EX_STATICEDGE = 0x00020000,
+      WS_EX_TOOLWINDOW = 0x00000080,
+      WS_EX_TOPMOST = 0x00000008,
+      WS_EX_TRANSPARENT = 0x00000020,
+      WS_EX_WINDOWEDGE = 0x00000100,
+    }
+
+    internal enum WindowLong : int
+    {
+      GWL_WNDPROC = -4,
+      GWL_HINSTANCE = -6,
+      GWL_ID = -12,
+      GWL_STYLE = -16,
+      GWL_EXSTYLE = -20,
+      GWL_USERDATA = -21,
+    }
+
+    internal enum InputType : uint
+    {
+      INPUT_MOUSE = 0,
+      INPUT_KEYBOARD = 1,
+      INPUT_HARDWARE = 2,
+    }
+
+    [Flags]
+    internal enum KeyboardInputFlags : uint
+    {
+      None = 0,
+      KEYEVENTF_EXTENDEDKEY = 0x00000001,
+      KEYEVENTF_KEYUP = 0x00000002,
+      KEYEVENTF_UNICODE = 0x00000004,
+      KEYEVENTF_SCANCODE = 0x00000008,
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct Input
+    {
+      [FieldOffset(0)]
+      public InputType type;
+      [FieldOffset(4)]
+      public MouseInput mi;
+      [FieldOffset(4)]
+      public KeyboardInput ki;
+      [FieldOffset(4)]
+      public HardwareInput hi;
+    }
+
+    internal struct MouseInput
+    {
+      public int dx;
+      public int dy;
+      public MouseInputData mouseData;
+      public MouseInputFlags dwFlags;
+      public uint time;
+      public UIntPtr dwExtraInfo;
+    }
+
+    internal enum MouseInputData : uint
+    {
+      Zero = 0,
+      XBUTTON1 = 0x00000001,
+      XBUTTON2 = 0x00000002,
+    }
+
+    [Flags]
+    internal enum MouseInputFlags : uint
+    {
+      None = 0,
+      MOUSEEVENTF_ABSOLUTE = 0x00008000,
+      MOUSEEVENTF_HWHEEL = 0x00001000,
+      MOUSEEVENTF_MOVE = 0x00000001,
+      MOUSEEVENTF_MOVE_NOCOALESCE = 0x00002000,
+      MOUSEEVENTF_LEFTDOWN = 0x00000002,
+      MOUSEEVENTF_LEFTUP = 0x00000004,
+      MOUSEEVENTF_RIGHTDOWN = 0x00000008,
+      MOUSEEVENTF_RIGHTUP = 0x00000010,
+      MOUSEEVENTF_MIDDLEDOWN = 0x00000020,
+      MOUSEEVENTF_MIDDLEUP = 0x00000040,
+      MOUSEEVENTF_VIRTUALDESK = 0x00004000,
+      MOUSEEVENTF_WHEEL = 0x00000800,
+      MOUSEEVENTF_XDOWN = 0x00000080,
+      MOUSEEVENTF_XUP = 0x00000100,
+    }
+
+    internal struct KeyboardInput
+    {
+      public ushort wVk;
+      public ushort wScan;
+      public KeyboardInputFlags dwFlags;
+      public uint time;
+      public UIntPtr dwExtraInfo;
+    }
+
+    internal struct HardwareInput
+    {
+      public uint uMsg;
+      public short wParamL;
+      public short wParamH;
+    }
+
+    [DllImport("user32.dll", SetLastError = true)]
+    internal static extern WindowStyle GetWindowLong(IntPtr hWnd, WindowLong nIndex);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    internal static extern int SetWindowLong(IntPtr hWnd, WindowLong nIndex, WindowStyle dwNewLong);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    internal static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
+  }
+}
